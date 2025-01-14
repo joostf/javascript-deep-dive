b7b52132160c870c5f64efc9b9cd3245f1cdf715
joostf <j.faber@hva.nl>
2025-01-14 11:54:08 +0100

    refactored code

--- static/js/script-functional.js
+++ static/js/script-functional.js
@@ -1,14 +1,14 @@
 // Immutability: We treat the emojis array as immutable.
-let emojis = ["😊", "😂", "🥺", "❤️", "😎", "🌟", "🌈", "🔥", "💡", "😜"];
+let emojis = ["😊", "😂", "🥺", "❤️", "😎", "🌟", "🌈", "🔥", "💡", "😜"]
 const createEmojiBtn = document.getElementById('generate-btn')
-const emojiForm = document.getElementById('emoji-form');
-const emojiInput = document.getElementById('emoji-input');
-const message = document.querySelector('.message');
+const emojiForm = document.getElementById('emoji-form')
+const emojiInput = document.getElementById('emoji-input')
+const message = document.querySelector('.message')
 
 // Event Handling: Attach an event listener to generate a new emoji on button click.
 createEmojiBtn.addEventListener('click', function() {
-  updateDisplay(getFormattedEmojiEmoji()); // Passing the callback function for formatting
-});
+  updateDisplay(getFormattedEmoji()) // Passing the callback function for formatting
+})
 
 // Defensive Programming: Add event listener to the form
 emojiForm.addEventListener('submit', function(event) {
@@ -24,56 +24,56 @@ emojiForm.addEventListener('submit', function(event) {
     addEmoji(newEmoji)
     emojiInput.value = '' // Clear the input field after adding
     message.textContent = 'Emoji added successfully'
+
   } catch (error) {
     message.textContent = String(error.message) // Provide feedback to the user with an alert
   } finally {
     event.preventDefault() // Always prevent default form submission after handling the logic
   }
-});
+})
 
 // Function declarations
 function getRandomEmoji() {
   // Pure Functions: getRandomEmoji is a pure function; it only relies on the input and does not cause side effects.
-  if (!emojis || !emojis.length) {
-    throw new Error('Emojis array is empty or null');
-  }
+  if (!emojis || !emojis.length) throw new Error('Emojis array is empty or null')
+  
   const randomIndex = Math.floor(Math.random() * emojis.length);
   return emojis[randomIndex]; // Pure function returns a random emoji
 }
 
 function getEmojiInfo() {
   // Object Destructuring: Destructuring example - destructure an emoji object to access its value.
-  const emoji = getRandomEmoji();
-  return { emoji, description: "Random Emoji" }; // Return an object with the emoji and description
+  const emoji = getRandomEmoji()
+  return { emoji, description: "Random Emoji" } // Return an object with the emoji and description
 }
 
-function getFormattedEmojiEmoji() {
+function getFormattedEmoji() {
   // First-Class Functions: Returning a function that formats the emoji with additional info.
-  return (emojiObject) => `Emoji: ${emojiObject.emoji} - Description: ${emojiObject.description}`; // using a string literal for formatting the emoji + description
+  return (emojiObject) => `Emoji: ${emojiObject.emoji} - Description: ${emojiObject.description}` // using a string literal for formatting the emoji + description
 }
 
 function updateDisplay(callback) {
   // Callback Functions: Pass a callback to format the emoji
   try {
-    const emojiInfo = getEmojiInfo();
-    const formattedEmoji = callback(emojiInfo);
-    const emojiDisplay = document.getElementById('emoji-display');
-    emojiDisplay.textContent = formattedEmoji;
+    const emojiInfo = getEmojiInfo()
+    const formattedEmoji = callback(emojiInfo)
+    const emojiDisplay = document.getElementById('emoji-display')
+    emojiDisplay.textContent = formattedEmoji
   } catch (error) {
-    console.error(error);
-    const emojiDisplay = document.getElementById('emoji-display');
-    emojiDisplay.textContent = 'Error: ' + error.message;
+    console.error(error)
+    const emojiDisplay = document.getElementById('emoji-display')
+    emojiDisplay.textContent = 'Error: ' + error.message
   }
 }
 
 function addEmoji(newEmoji) {
   // Immutability: Using the spread operator to create a new array with the new emoji added
-  emojis = [...emojis, newEmoji]; // Spread the existing emojis and add the new emoji
+  emojis = [...emojis, newEmoji] // Spread the existing emojis and add the new emoji
 }
 
 // Function to check if a string is an emoji
 function isEmoji(inputStr) {
   // Regular expression to match a wide range of emojis
-  const emojiRegex = /[\p{Emoji}\u200B\uFE0F]/gu;
-  return emojiRegex.test(inputStr);
+  const emojiRegex = /[\p{Emoji}\u200B\uFE0F]/gu
+  return emojiRegex.test(inputStr)
 }
--- workshop-javascript-deep-dive.md
+++ workshop-javascript-deep-dive.md
@@ -11,17 +11,19 @@
 * Making it more fun to write JavaScript
 * Needed skills to land a job:  [10 Interview Questions Every JavaScript Developer Should Know in 2024]
 * How to handle your Imposter Syndrome
-* How to handle bro coding culture
+* How to handle bro coding culture, how to survive the complexity of the web
 
 ### Theory based on two demo's: a functional - and an object-oriented one
 
-The main concepts derived from the code:
-* Fundamentals of JavaScript (Constructor Functions, this, Scope)
+The main concepts derived from the demo code:
+* Fundamentals of JavaScript (Constructor Functions, Context(this), Scope)
 * Functions and Closures (Callbacks, Returning Functions, Binding Functions)
 * Array Manipulation and Logical Operations (Array Methods, Short-Circuiting, Event Delegation)
 * Async Programming in JavaScript (Promises, Async/Await)
 * Code Flow and Control Structures (Defensive Programming, Return Early, object destructuring, spread operator)
 * Advanced JavaScript Concepts (Chaining Methods, Concurrency Model, Event Loop (Call Stack, Event Queue, and Microtask Queue), Hoisting, Closure)
+
+Bron: You Don't Know JS
 
 
 Topics we encounter when using the debugger from the **Sources** tab in DevTools:
@@ -29,7 +31,7 @@ Topics we encounter when using the debugger from the **Sources** tab in DevTools
 * Scope 
 * Context (this)
 * Call Stack (Concurrency Model / Single thread)
-* Extra: modules (scoped, deferred, async)
+* Extra GSAP DEMO: modules (scoped, deferred, async)
 
 
 ### Practical applications in a SvelteKit Project
@@ -37,9 +39,11 @@ Topics we encounter when using the debugger from the **Sources** tab in DevTools
 Opdracht Refactor your JavaScript:
 * Object destructuring
 * Short circuiting
+* Spread operator
 * One responsibility rule for functions
-* Pseudo-Private Custom Properties (CSS)
+* Pseudo-Private Custom Properties ('Scoped' CSS) 
 
+Decide which coventions you apply to your work and share/discuss them with your team mates
 
 <!-- references -->
 [10 Interview Questions Every JavaScript Developer Should Know in 2024]: https://medium.com/javascript-scene/10-interview-questions-every-javascript-developer-should-know-in-2024-c1044bcb0dfb
